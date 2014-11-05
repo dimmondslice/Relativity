69b2b5898b30dc97747617bdce1156fa14bb0e69
Spencer <johnss18@rpi.edu>
2014-11-05 02:55:37 -0500

    I don't believe you Kevan

---
+++ TeamPlaceHolderGame/Assets/Animations/PathNode.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathNode : MonoBehaviour
+{
+	public PathNode nextNode;
+
+	public static List<PathNode> allPathNodes;
+
+	void Awake () 
+	{
+		//initialize allPathNodes if it hasn't been initialized by any other existing node yet
+		if(allPathNodes == null)
+			allPathNodes = new List<PathNode>();
+
+		//add myself to the list of all pathnodes
+		allPathNodes.Add (this);
+
+		//set the nextnode in the path
+		//nextNode = FindClosestNodeWithNullPTR(transform);
+	}
+	public static PathNode FindClosestNode(Transform t)
+	{
+		PathNode currentClosest = PathNode.allPathNodes[0];		//arbitrarily make currentclosest node the first nod in the list so it isn't null
+
+		foreach(PathNode node in PathNode.allPathNodes)
+		{
+			if(Vector3.Distance(t.position, node.transform.position) < Vector3.Distance(t.position, currentClosest.transform.position))
+			{
+				currentClosest = node;		//update what is stored as the closest node to t
+			}
+		}
+		return currentClosest;
+	}
+
+
+
+
+	public static PathNode FindClosestNodeWithNullPTR(Transform t)
+	{
+		PathNode currentClosest = PathNode.allPathNodes[0];		//arbitrarily make currentclosest node the first nod in the list so it isn't null
+		
+		foreach(PathNode node in PathNode.allPathNodes)
+		{
+			if(Vector3.Distance(t.position, node.transform.position) < Vector3.Distance(t.position, currentClosest.transform.position))
+			{
+				if(node.nextNode == null && node != t.GetComponent<PathNode>())				///if the ptr isnt null or is just the node who called this func, then ignore it
+					currentClosest = node;		//update what is stored as the closest node to t
+			}
+		}
+		return currentClosest;
+	}
+}
--- TeamPlaceHolderGame/Assets/GuyCharacter.cs
+++ TeamPlaceHolderGame/Assets/GuyCharacter.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GuyCharacter : Character
 {
+	//the next node
+	public Transform target;
+
 	void Start ()
 	{
 	
@@ -10,6 +13,31 @@ public class GuyCharacter : Character
 
 	protected override void Update () 
 	{
-	
+		/*
+		float distThreshold = 2f;
+
+		if(target == null)
+			target = PathNode.FindClosestNode(transform).transform;
+		
+		if(Vector3.Distance(transform.position, target.position) < distThreshold)
+		{
+			if(target.GetComponent<PathNode>().nextNode == null)					//if you run out of nodes to follow just start to wander around
+			{
+				//need to destroy this instance
+			}
+			//otherwise just make the next node your target
+			target = target.GetComponent<PathNode>().nextNode.transform;
+		}
+		//otherwise increase your speed
+		else
+		{
+			//if(currentSpeed < maxSpeed)
+				//currentSpeed += agent.accel;
+		}
+		//then actually move yourself
+		TurnTowards(target.position);
+		rigidbody.velocity = new Vector3(transform.forward.x * agent.currentSpeed, rigidbody.velocity.y, transform.forward.z * agent.currentSpeed);
+		*/
 	}
+	
 }
